6643aba3ec7220f84f7a5fa36fe70de6113a483a
Jasf <bazoocaze@gmail.com>
2026-07-21 21:31:53 -0300

    Initial commit: testlib1 - simple library that returns 42

---
+++ src/main/java/com/github/bazoocaze/lib/App.java
@@ -0,0 +1,19 @@
+package com.github.bazoocaze.lib;
+
+/**
+ * Simple library that returns the answer to life, the universe and everything.
+ */
+public class App {
+
+    /**
+     * Returns the answer to life, the universe and everything.
+     * @return 42
+     */
+    public static int getAnswer() {
+        return 42;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getAnswer());
+    }
+}
